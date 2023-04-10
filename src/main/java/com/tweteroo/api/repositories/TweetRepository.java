756bdd8de72e2d53db3c3afecf81e986a36af14a
Vinícius Barros <vicaetano_barros@hotmail.com>
2023-04-10 16:33:44 -0300

    feat: add tweets return function

--- src/main/java/com/tweteroo/api/repositories/TweetRepository.java
+++ src/main/java/com/tweteroo/api/repositories/TweetRepository.java
@@ -15,4 +15,20 @@ public class TweetRepository {
     tweets.add(tweet);
   }
 
+  public List<Tweet> getAllTweets(int pag) {
+    int finalI = tweets.size() - (pag * 5);
+    int initialI = finalI + 4;
+
+    if(finalI < 0) finalI = 0;
+    if(initialI >= tweets.size()) initialI = tweets.size() - 1;
+
+    List<Tweet> filteredTweets = new ArrayList<>();
+
+    for (int i = initialI; i >= finalI; i--) {
+      filteredTweets.add(tweets.get(i));
+    }
+
+    return filteredTweets;
+  } 
+
 }
--- src/main/java/com/tweteroo/api/services/TweetService.java
+++ src/main/java/com/tweteroo/api/services/TweetService.java
@@ -1,5 +1,7 @@
 package com.tweteroo.api.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -22,5 +24,9 @@ public class TweetService {
     if (avatar.equals("Not found")) throw new Error("User doesn't exist");
 
     tweetRepository.postNew(new Tweet(tweet.username(), avatar, tweet.text()));
+  }
+
+  public List<Tweet> tweets(int pag) {
+    return tweetRepository.getAllTweets(pag);
   }
 }
